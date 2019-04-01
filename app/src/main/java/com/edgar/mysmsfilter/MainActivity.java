f5d332d34c7a7c004b7eae800c45ee361708a78d
Edgar <1452192394@qq.com>
2019-04-01 20:42:56 +0800

    Basic Ui3

--- app/src/main/java/com/edgar/mysmsfilter/MainActivity.java
+++ app/src/main/java/com/edgar/mysmsfilter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.edgar.mysmsfilter;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
@@ -22,10 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private ContentObserver mObserver;
     private Button btnAddConfirm;
     private Button btnViewAll;
+    private final ArrayList<String> finalKeywords = new ArrayList<>();
+    private Button btnReset;
     private EditText etInputWord;
     private TextView tvShowAll;
     private String defaultKeywords[] = {"服务厅"};
-    private ArrayList<String> finalKeywords = new ArrayList<>();
+    private Button btnSave;
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -35,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
                     if (inputWord.length() > 0) {
                         finalKeywords.add(etInputWord.getText().toString());
                         etInputWord.setText("");
-                        Toast.makeText(MainActivity.this, "Text added: "
-                                + etInputWord.getText().toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Text added: " + inputWord,
+                                Toast.LENGTH_SHORT).show();
                     }
                     break;
 
@@ -46,6 +50,15 @@ public class MainActivity extends AppCompatActivity {
                         outputWords = outputWords.concat(str + ", ");
                     }
                     tvShowAll.setText(outputWords);
+                    break;
+
+                case R.id.btn_reset:
+                    finalKeywords.clear();
+                    finalKeywords.addAll(Arrays.asList(defaultKeywords));
+                    break;
+
+                case R.id.btn_save:
+                    saveWords(getApplicationContext(), finalKeywords);
                     break;
 
                 default:
@@ -59,14 +72,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        finalKeywords.addAll(Arrays.asList(defaultKeywords));
         btnAddConfirm = findViewById(R.id.btn_add_confirm);
         btnViewAll = findViewById(R.id.btn_view_all);
+        btnSave = findViewById(R.id.btn_save);
+        btnReset = findViewById(R.id.btn_reset);
         etInputWord = findViewById(R.id.et_enter_words);
         tvShowAll = findViewById(R.id.tv_show_all);
 
         btnAddConfirm.setOnClickListener(mOnClickListener);
         btnViewAll.setOnClickListener(mOnClickListener);
+        btnReset.setOnClickListener(mOnClickListener);
+        btnSave.setOnClickListener(mOnClickListener);
 
         mObserver = new ContentObserver(new Handler()) {
             @Override
@@ -101,8 +117,51 @@ public class MainActivity extends AppCompatActivity {
                 cursor.close();
             }
         };
-
         getContentResolver().registerContentObserver(Uri.parse("content://sms/"), true, mObserver);
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ArrayList<String> temp = getWords(getApplicationContext());
+        finalKeywords.clear();
+        if (temp == null) {
+            finalKeywords.addAll(Arrays.asList(defaultKeywords));
+            Toast.makeText(this, "Empty SharedPreference!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Non empty SharedPreference!", Toast.LENGTH_SHORT).show();
+            finalKeywords.addAll(temp);
+        }
+    }
+
+    private void saveWords(Context context, ArrayList<String> wordsList) {
+        if (wordsList.size() == 0) return;
+        SharedPreferences sharedPreferences = context.getSharedPreferences("KEYWORDS_LIST",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("LENGTH", wordsList.size());
+        for (String word : wordsList) {
+            editor.putString("INDEX_" + String.valueOf(wordsList.indexOf(word)), word);
+        }
+        Toast.makeText(context, "All keywords saved!", Toast.LENGTH_SHORT).show();
+        editor.apply();
+    }
+
+    private ArrayList<String> getWords(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("KEYWORDS_LIST",
+                Context.MODE_PRIVATE);
+        int length = sharedPreferences.getInt("LENGTH", 0);
+        if (length == 0) return null;
+
+        final ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            String key = "INDEX_" + String.valueOf(i);
+            result.add(i, sharedPreferences.getString(key, "ERROR_EMPTY_WORD"));
+        }
+        return result;
+    }
+
 }
